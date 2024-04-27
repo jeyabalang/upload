297c012ad6dd7f2b62eb11be9f7aac6227e48b69
jeyabalang <jeyabalan360@gmail.com>
2024-04-27 06:16:15 +0200

    Update Final-Stage-Router-Maths-Coding.py
    I have attached the code with the custom model and the input of the model to chat with a coding, math routing customize o streamline application

--- Final-Stage-Router-Maths-Coding.py
+++ Final-Stage-Router-Maths-Coding.py
@@ -5,78 +5,65 @@ import os
 from semantic_router import Route
 from getpass import getpass
 from semantic_router import RouteLayer
+from concurrent.futures import ThreadPoolExecutor
 from semantic_router.encoders import OpenAIEncoder
 
 
-# Asynchronously handle chat operations
+# Routes to the appropriate endpoint
+async def semantic_route(api_key, route_endpoint, user_input):
+    unify = AsyncUnify(
+        api_key=api_key,
+        # Use the correct endpoint for math queries
+        endpoint=route_endpoint
+    )
+   # Generate the response using Unify
+    response = await unify.generate(user_prompt=user_input)
+   # If response is a string and not a stream, handle it directly
+    if isinstance(response, str):
+        return response
 
-async def async_chat(openai_api_key, api_key, user_input, routes):
-    # encoder = CohereEncoder()
+   # If response is a stream, then iterate over it
+    response_text = ''
+    async for chunk in response:
+        response_text += chunk
+    return response_text
+
+# Re-implemented async_chat to include custom endpoints.
+
+
+async def async_chat(openai_api_key, api_key, user_input, routes, endpoint="llama-2-13b-chat"):
+    # Set API key environment variable at the beginning of the function, if not set globally
     os.environ["OPENAI_API_KEY"] = openai_api_key
+    print(f"routes in async_chat:{routes}")
+    print(f"endpoint chosen:{endpoint}")
+    # Assuming OpenAIEncoder and RouteLayer are defined and imported properly elsewhere
     encoder = OpenAIEncoder()
-
     rl = RouteLayer(encoder=encoder, routes=routes)
     route_choice = rl(user_input)
-    print(f"Route chosen: {route_choice}")
+    print(f"Route chosen: {route_choice.name}")
 
-    if route_choice.name == "math":
-        # Initialize Unify with the endpoint for math queries
-        unify = AsyncUnify(
-            api_key=api_key,
-            # Use the correct endpoint for math queries
-            endpoint="gpt-4"
-        )
-        # Generate the response using Unify
-        response = await unify.generate(user_prompt=user_input)
-        # If response is a string and not a stream, handle it directly
-        if isinstance(response, str):
-            return response
+    # Define specific endpoints for known route names
+    endpoint_map = {
+        "math": "llama-2-13b-chat",
+        "coding": "codellama-34b-instruct"
+    }
 
-        # If response is a stream, then iterate over it
-        response_text = ''
-        async for chunk in response:
-            response_text += chunk
-        return response_text
-
-    elif route_choice.name == "coding":
-        # Initialize Unify with the endpoint for coding queries
-        unify = AsyncUnify(
-            api_key=api_key,
-            # Use the correct endpoint for coding queries
-            endpoint="deepseek-coder-33b-instruct"
-        )
-        # Generate the response using Unify
-        response = await unify.generate(user_prompt=user_input)
-        # If response is a string and not a stream, handle it directly
-        if isinstance(response, str):
-            return response
-
-        # If response is a stream, then iterate over it
-        response_text = ''
-        async for chunk in response:
-            response_text += chunk
-        return response_text
-
+    # Check if the route name is in the endpoint map, otherwise use the user-provided endpoint
+    if route_choice.name in endpoint_map:
+        chosen_endpoint = f"{endpoint_map[route_choice.name]}@anyscale"
     else:
-        unify = AsyncUnify(
-            api_key=api_key,
-            # Use the correct endpoint for coding queries
-            endpoint="llama-2-13b-chat@anyscale"
-        )
-        # Generate the response using Unify
-        response = await unify.generate(user_prompt=user_input)
-        # If response is a string and not a stream, handle it directly
-        if isinstance(response, str):
-            return response
+        # Append "@anyscale" if not already included
+        if "@anyscale" not in endpoint:
+            endpoint += "@anyscale"
+        chosen_endpoint = endpoint
 
-        # If response is a stream, then iterate over it
-        response_text = ''
-        async for chunk in response:
-            response_text += chunk
-        return response_text
+    # Call the semantic route function with the chosen endpoint
+    response = await semantic_route(api_key, chosen_endpoint, user_input)
+    return response
+
+# Define routes function
 
 
-#Define the routes with the maths and the coding work
 def defineRoutes():
     math_route = Route(
         name="math",
@@ -105,28 +92,65 @@ def defineRoutes():
     routes = [math_route, coding_route]
     return routes
 
-#Handle the send request to the session of it
-def handle_send():
-    user_input = st.session_state.user_input
-    route_list = defineRoutes()
-    if user_input:
-        st.session_state.history.append(f"You: {user_input}")
-        response = asyncio.run(async_chat(st.session_state.openai_api_key,
-                                          st.session_state.unify_key, user_input, route_list))
-        st.session_state.history.append(f"Bot: {response}")
-    # Clear the input field by resetting the state variable
-    st.session_state.user_input = ""
-    st.rerun()
-#unifyai
-#7wTNz+iEWsWIEdvuCtLR8ov1tjnHkUFfcwE5wLR3YWM=
 
-#openai
-#sk-proj-1pP1PtFQ0XNgLSlWLQN2T3BlbkFJtjtujIuIzCVJD60wQOzC
+# Custom routes function
+def customRoutes(route_name, route_examples, route_list):
+    custom_route = Route(
+        name=route_name,
+        utterances=route_examples.split(','),
+
+    )
+    print(f"custom route name:{custom_route.name}")
+    print(f"custom route utteraqnces:{custom_route.utterances}")
+    route_list.append(custom_route)
+    print(f"Route list:{route_list}")
+    return route_list
+# handles send
+
+
+def run_async_coroutine(coroutine):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    return loop.run_until_complete(coroutine)
+
+
+def async_chat_wrapper(user_input, openai_api_key, unify_key, routes, endpoint="llama-2-13b-chat"):
+    # Pass the default endpoint if not provided
+    coroutine = async_chat(openai_api_key, unify_key,
+                           user_input, routes, endpoint)
+    return run_async_coroutine(coroutine)
+
 
 def main():
+    # Assuming that 'defineRoutes' and 'async_chat_wrapper' are defined elsewhere
+
+    # Include Font Awesome
+    st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">', unsafe_allow_html=True)
+
     st.sidebar.title("Configuration")
     unify_key = st.sidebar.text_input("Enter your UNIFY_KEY", type='password')
     openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+    # Dropdown for model selection
+    model_list = [
+        "mixtral-8x7b-instruct-v0.1", "llama-2-70b-chat", "llama-2-13b-chat",
+        "mistral-7b-instruct-v0.2", "llama-2-7b-chat", "codellama-34b-instruct",
+        "gemma-7b-it", "mistral-7b-instruct-v0.1", "mixtral-8x22b-instruct-v0.1",
+        "codellama-13b-instruct", "codellama-7b-instruct", "yi-34b-chat",
+        "llama-3-8b-chat", "llama-3-70b-chat", "pplx-7b-chat", "mistral-medium",
+        "gpt-4", "pplx-70b-chat", "gpt-3.5-turbo", "deepseek-coder-33b-instruct",
+        "gemma-2b-it", "gpt-4-turbo", "mistral-small", "mistral-large",
+        "claude-3-haiku", "claude-3-opus", "claude-3-sonnet"
+    ]
+
+    custom_element = st.sidebar.checkbox("Custom input?")
+
+    if custom_element:
+        custom_route_name = st.sidebar.text_input(
+            "Enter the name of your custom route:")
+        custom_utterances = st.sidebar.text_input(
+            "Enter some examples to direct to this route (separate by comma):")
+        selected_model = st.sidebar.selectbox(
+            "Select a model for your custom route:", model_list)
 
     if openai_api_key and not openai_api_key.startswith('sk-'):
         st.sidebar.warning('Please enter a valid OpenAI API key!', icon='⚠️')
@@ -134,23 +158,38 @@ def main():
     if unify_key and openai_api_key.startswith('sk-'):
         st.session_state.unify_key = unify_key
         st.session_state.openai_api_key = openai_api_key
-        st.title("Streaming Router ChatBot")
+        st.title("🤖💬 Streaming Router ChatBot")
 
-        if 'history' not in st.session_state:
-            st.session_state.history = []
+        # Initialize or update the chat history in session state
+        if 'chat_history' not in st.session_state:
+            st.session_state.chat_history = []
 
-        for message in st.session_state.history:
-            sender, text = message.split(": ", 1)
-            st.text(f"{sender}: {text}")
+        # Display existing chat messages
+        messages_container = st.container()
+        for msg_type, msg_content in st.session_state.chat_history:
+            if msg_type == "user":
+                messages_container.chat_message("user").write(msg_content)
+            elif msg_type == "assistant":
+                messages_container.chat_message("assistant").write(msg_content)
 
-        # The user_input is now tied to the text input's state directly
-        st.session_state.user_input = st.text_input(
-            "Type your message here:", value=st.session_state.get('user_input', ''))
+        # Chat input at the bottom of the page
+        user_input = st.chat_input("Say something", key="chat_input")
 
-        if st.button("Send", on_click=handle_send):
-            # Button press is handled in the handle_send function
-            pass
+        if user_input:
+            routes = defineRoutes()  # Assuming defineRoutes is defined to handle routing logic
+            if custom_element:
+                routes = customRoutes(
+                    custom_route_name, custom_utterances, routes)
 
+            with ThreadPoolExecutor() as executor:
+                future = executor.submit(
+                    async_chat_wrapper, user_input, st.session_state.openai_api_key, st.session_state.unify_key, routes, selected_model)
+                response = future.result()
+                # Update the session state with the new messages
+                st.session_state.chat_history.append(("user", user_input))
+                st.session_state.chat_history.append(("assistant", response))
+                # Rerun the app to update the UI
+                st.experimental_rerun()
     else:
         st.error("Please enter valid keys to start chatting.")
 
