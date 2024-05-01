988b47cd8627b34866c7e32cd5926d92acf118e9
jeyabalang <jeyabalan360@gmail.com>
2024-05-01 12:22:29 +0200

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,34 @@
+### file launch_dashboard.py ####
+import os
+import asyncio
+import streamlit.bootstrap
+from datetime import datetime
+import nest_asyncio
+
+nest_asyncio.apply()
+
+
+async def do_some_iterations():
+    for i in range(10):
+        print(datetime.now().time())
+        await asyncio.sleep(1)
+    print('... Cool!')
+
+
+async def main():
+    task = asyncio.create_task(do_some_iterations())
+    task2 = asyncio.create_task(load_dashboard())
+    await task
+    await task2
+
+async def load_dashboard():
+    print('Loading dashboard')
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'dummy.py')
+    args = []
+    streamlit.bootstrap.run(filename, '', args, flag_options={})
+    print('Loaded.')
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
